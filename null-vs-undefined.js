4228d9fa3ff0eedf51652b0b273fdd04cf01ce17
Naeem-Ul-Hoque <nuhreyad@gmail.com>
2021-05-24 21:17:33 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+let pakhi;
+function add(num1, num2){
+console.log(num1 + num2);
+return
+}
+
+function add(num1, num2){
+console.log(num1,num2);
+}
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
